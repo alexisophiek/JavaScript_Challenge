8fbaec712d24bc851e70c864d304eac0c54f2ce4
Alexis <alexisophie.k@gmail.com>
2019-12-16 17:23:58 -0800

    Level One!

---
+++ UFO_Level_1/js/app.js
@@ -0,0 +1,23 @@
+// Variable Declaration
+let tbody = d3.select("tbody");
+// From data.js
+var tableData = data;
+
+// Function Build Table
+function buildTable(data){
+    // Start By Clearing Existing Data
+    tbody.html("");
+    // Loop Through `data` 
+    data.forEach((dataRow) => {
+        // Append Table Row `tr` to the Table Body `tbody`
+        let row = tbody.append("tr");
+        // `Object.values` & `forEach` to Iterate Through Values
+        Object.values(dataRow).forEach((val) => {
+            // Append a Cell to the Row for Each Value
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    })
+}
+// Build Table with data.js 
+buildTable(tableData);
